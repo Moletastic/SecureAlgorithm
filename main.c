e057906bc0a02df9f9f3e4c55661f35379e36b52
Diego Sepúlveda <diegosepu.soto@gmail.com>
2019-04-28 00:06:29 -0400

    lectura desde archivos realizada

--- main.c
+++ main.c
@@ -5,111 +5,149 @@
 #include "utils/matrix.h"
 #include "utils/colors.h"
 
+void inicializacionTabla(TaskTable* table, char archivo[]);
+
 int main(){
     
     WORD defecto = getInfo();
     HANDLE hConsole = getConsoleData();
-    // int procesos = 5;
-    // int recursos = 3;
-    // short i,j;
-    // int input_assign[5][3] = {
-    //     {1,0,0}, 
-    //     {0,1,2}, 
-    //     {0,0,3}, 
-    //     {1,0,3}, 
-    //     {1,1,0}
-    // };
 
-    // int input_max[5][3] = {
-    //     {4,4,4}, 
-    //     {1,4,6}, 
-    //     {3,2,5}, 
-    //     {4,4,8}, 
-    //     {5,1,10}
-    // };
-
-    // int input_required[5][3] = {
-    //     {3,4,4},
-    //     {1,3,4},
-    //     {3,2,2},
-    //     {3,4,5},
-    //     {4,0,10}
-    // };
-
-    // bool input_solved[5] = {false, false, false, false, false};
-    // int input_init_memo[3] = {3,3,2};
-
-    int procesos = 5;
-    int recursos = 3;
-    short i,j;
-    int input_assign[5][3] = {
-        {0,1,0}, 
-        {2,0,0}, 
-        {3,0,2}, 
-        {2,1,1}, 
-        {0,0,2}
-    };
-
-    int input_max[5][3] = {
-        {7,5,3}, 
-        {3,2,2}, 
-        {9,0,2}, 
-        {2,2,2}, 
-        {4,3,3}
-    };
-
-    int input_required[5][3] = {
-        {7,4,3},
-        {1,2,2},
-        {6,0,0},
-        {0,1,1},
-        {4,3,1}
-    };
-
-    bool input_solved[5] = {false, true, false, false, false};
-    int input_init_memo[3] = {3,3,2};
-
-    // Inicializaciones /
+    // // Inicializaciones /
     TaskTable* table = malloc(sizeof(TaskTable));
-    table->assign = (int**)malloc(procesos * sizeof(int*));
-    table->max = (int**)malloc(procesos * sizeof(int*));
-    table->required = (int**)malloc(procesos * sizeof(int*));
-    table->solved = (bool*)malloc(procesos * sizeof(bool));
-    table->init_memo = (int*)malloc(recursos* sizeof(int));
-    table->memo = (int*)malloc(recursos* sizeof(int));
-    table->n_process = procesos;
-    table->n_resources = recursos; 
+    inicializacionTabla(table, "procesos-recursos/ejemplo1.txt");
 
-    for(i = 0; i < procesos; i++){
-        table->assign[i] = (int*)malloc(recursos* sizeof(int));
-        table->max[i] = (int*)malloc(recursos* sizeof(int));
-        table->required[i] = (int*)malloc(recursos* sizeof(int));
-    }
-
-    for(i = 0; i < procesos; i++){
-        for(j = 0; j < recursos; j++){
-            table->assign[i][j] = input_assign[i][j];
-            table->max[i][j] = input_max[i][j];
-            table->required[i][j] = input_required[i][j];
-        }
-    }
-    for(i = 0; i < procesos; i++){
-        table->solved[i] = input_solved[i];
-    }
-
-    for(i = 0; i < recursos; i++){
-        table->init_memo[i] = input_init_memo[i];
-        table->memo[i] = input_init_memo[i];
-    }
-
-    // Muestra de los procesos
-    // showTaskTable(table, -99);
-
+    // // Muestra de los procesos
+    // // showTaskTable(table, -99);
 
     // Inicio Algoritmo /
     SecureAlgorithm(table);
     setColor(hConsole, defecto);
+
     return 0;
 }
 
 
+void inicializacionTabla(TaskTable* table, char archivo[]) {
+
+    int i, j;
+    FILE *f = fopen(archivo, "r");
+    char string[100];
+
+    char *ptr;
+
+    int cant_proc;
+    int cant_rec;
+    int recursos_ini[100];
+    int recursos_asig[100][100];
+    int recursos_max[100][100];
+    char delim[] = ";";
+
+    if(f == NULL) {
+        printf("No se puede abrir el archivo ...");
+        return;
+    }
+
+    if(fgets(string, 100, f) != NULL) {
+        ptr = strtok(string, delim);
+        cant_proc = atoi(ptr);
+        if(cant_proc < 1 || cant_proc > 10){
+            printf("No se puede abrir el archivo ...");
+            return;
+        } 
+    }
+    else {
+        printf("No se puede abrir el archivo ...");
+        return;
+    }
+
+    if(fgets(string, 100, f) != NULL) {
+        ptr = strtok(string, delim);
+        cant_rec = atoi(ptr);
+        if(cant_rec < 1 || cant_rec > 10){
+            printf("No se puede abrir el archivo ...");
+            return;
+        } 
+    }
+    else {
+        printf("No se puede abrir el archivo ...");
+        return;
+    }
+
+    if(fgets(string, 100, f) != NULL) {
+        ptr = strtok(string, delim);
+        recursos_ini[0] = atoi(ptr);
+        if(recursos_ini[0] < 0){
+            printf("No se puede abrir el archivo ...");
+            return;
+        }
+        for(i = 1; i < cant_rec; i++) {
+            ptr = strtok(NULL, delim);
+            recursos_ini[i] = atoi(ptr);
+        }
+    }
+    else {
+        printf("No se puede abrir el archivo ...");
+        return;
+    }
+
+    for(int i = 0; i < cant_proc; i++) {
+        if(fgets(string, 100, f) != NULL) {
+            ptr = strtok(string, delim);
+            recursos_asig[i][0] = atoi(ptr);
+            if(recursos_asig[i][0] < 0){
+                printf("No se puede abrir el archivo ...");
+                return;
+            }
+            for(j = 1; j < cant_rec; j++) {
+                ptr = strtok(NULL, delim);
+                recursos_asig[i][j] = atoi(ptr);
+            }
+            ptr = strtok(NULL, delim);
+            recursos_max[i][0] = atoi(ptr);        
+            if(recursos_max[i][0] < 0){
+                printf("No se puede abrir el archivo ...");
+                return;
+            }
+            for(j = 1; j < cant_rec; j++) {
+                ptr = strtok(NULL, delim);
+                recursos_max[i][j] = atoi(ptr);
+            }
+        }
+        else {
+            printf("No se puede abrir el archivo ...");
+            return;
+        }
+    } 
+
+    table->n_process = cant_proc;
+    table->n_resources = cant_rec; 
+    table->assign = (int**)malloc(cant_proc * sizeof(int*));
+    table->max = (int**)malloc(cant_proc * sizeof(int*));
+    table->required = (int**)malloc(cant_proc * sizeof(int*));
+    table->solved = (bool*)malloc(cant_proc * sizeof(bool));
+    table->init_memo = (int*)malloc(cant_rec* sizeof(int));
+    table->memo = (int*)malloc(cant_rec* sizeof(int));
+
+    for(i = 0; i < cant_proc; i++){
+        table->assign[i] = (int*)malloc(cant_rec* sizeof(int));
+        table->max[i] = (int*)malloc(cant_rec* sizeof(int));
+        table->required[i] = (int*)malloc(cant_rec* sizeof(int));
+    }
+
+    for(i = 0; i < cant_proc; i++){
+        for(j = 0; j < cant_rec; j++){
+            table->assign[i][j] = recursos_asig[i][j];
+            table->max[i][j] = recursos_max[i][j];
+            table->required[i][j] = table->max[i][j] - table->assign[i][j];
+        }
+    }
+    for(i = 0; i < cant_proc; i++){
+        table->solved[i] = false;
+    }
+
+    for(i = 0; i < cant_rec; i++){
+        table->init_memo[i] = recursos_ini[i];
+        table->memo[i] = recursos_ini[i];
+    }
+
+}
